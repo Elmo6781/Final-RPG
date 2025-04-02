b517ef0d5641eaf1f491ba73974ee27666b9f6a2
Saurya-pixel <sauri.bachala@gmail.com>
2025-04-02 17:03:05 +0000

    added carnivore and herbivore subclass

---
+++ Carnivore.java
@@ -0,0 +1,26 @@
+public class Carnivore extends Dino{
+    private String info;
+
+    public Carnivore(int a, String n, String e, String i){
+        super(a, n, e);
+        info = i;
+    }
+    
+    public String getInfo(){
+        Return info; 
+    }
+    public void setInfo(int newI){
+        Info = newI; 
+    }
+    @Override         
+    public String toString(){
+        return "Welcome to the " + name + " paddock! This dinosaur is " + age + " years old originating from the " + era + "!" + info;
+    }
+
+}
+    
+    
+    
+    
+    
+    
---
+++ Herbivore.java
@@ -0,0 +1,20 @@
+public class Herbivore extends Dino{
+    private String info; 
+  
+    public Herbivore(int a, String n, String e, String i){
+      super(a, n, e);
+      info = i;
+  }
+  
+  public String getInfo(){
+      Return info; 
+  }
+  public void setInfo(int newI){
+      Info = newI; 
+  }
+  @Override
+  public String toString(){
+      return "Welcome to the " + name + " paddock! This dinosaur is " + age + " years old originating from the " + era + "!" + info;
+  }
+}
+  
