3669a553e36c6c4badd106f8ee332b4ccfd3a4a4
Elamarangit <ee8772@pleasantonusd.net>
2025-04-14 14:12:17 +0000

    main change

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String[] buildings = {"Incubators", "Paddocks", ""};
+
+
+        Create gamePark = new Create(null, args);
+
+
+
+
         
-        System.out.println("Hi Elmo");//
     }
 }
