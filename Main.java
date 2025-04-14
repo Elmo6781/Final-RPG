04b9a4b4454b4a1ede83c91c8792f3f687c62190
Elamarangit <ee8772@pleasantonusd.net>
2025-04-14 16:25:55 +0000

    Park creation main

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        String[] buildings = {"Incubators", "Paddocks", ""};
+        String[] buildings = {"I", "P", "R","M"};
+        String[][] P = new String[20][20];
+
+        
+        Create gamePark = new Create(P, buildings);
+        P = gamePark.PopulateArray();
 
 
-        Create gamePark = new Create(null, args);
 
 
 
